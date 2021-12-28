a563c7375545a3e87931f143690a089653fdacba
Marc-Antoine Ruel <maruel@gmail.com>
2021-12-28 15:56:53 -0500

    Fix compilation on Windows
    Make go generate only work on linux. That's sufficient for now.

--- depfile_parser.go
+++ depfile_parser.go
@@ -13,8 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:generate ./regen.sh
-
 package ginja
 
 type DepfileParserOptions struct {
