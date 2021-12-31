0beef5ceb475c50b939b087bf40500b0133fc2f8
Marc-Antoine Ruel <maruel@gmail.com>
2021-12-31 09:23:07 -0500

    Add --no-version to re2go
    This is unnecessary.

--- regen.sh
+++ regen.sh
@@ -37,11 +37,11 @@ elif [[ -f ../re2c/out/re2go ]]; then
 fi
 
 echo "Generating depfile_parser.go"
-re2go depfile_parser.in.go -o depfile_parser.go -i --no-generation-date
+re2go depfile_parser.in.go -o depfile_parser.go -i --no-generation-date --no-version
 sed -i -e "/go.\+neverbuild/d" depfile_parser.go
 gofmt -s -w depfile_parser.go
 
 echo "Generating lexer.go"
-re2go lexer.in.go -o lexer.go -i --no-generation-date
+re2go lexer.in.go -o lexer.go -i --no-generation-date --no-version
 sed -i -e "/go.\+neverbuild/d" lexer.go
 gofmt -s -w lexer.go
