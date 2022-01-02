maruel/nin

commit d30005bca3732d524e72d7d004166bc372a28504

Split CanonicalizePath in two; return slashbits instead of taking a pointer
It doesn't improve perf as much as I had hoped but it's non-zero, so
keeping.

name              old time/op    new time/op    delta
CLParser             291µs ± 0%     291µs ± 0%    ~     (p=0.684
n=10+10)
LoadManifest         481ms ± 2%     486ms ± 3%    ~     (p=0.113 n=9+10)
CanonicalizePath    91.4ns ± 1%    91.1ns ± 0%  -0.35%  (p=0.008
n=10+10)

name              old alloc/op   new alloc/op   delta
CLParser             167kB ± 0%     167kB ± 0%    ~     (all equal)
LoadManifest         440MB ± 0%     440MB ± 0%    ~     (p=0.075
n=10+10)
CanonicalizePath     80.0B ± 0%     80.0B ± 0%    ~     (all equal)

name              old allocs/op  new allocs/op  delta
CLParser             1.75k ± 0%     1.75k ± 0%    ~     (all equal)
LoadManifest         3.38M ± 0%     3.38M ± 0%    ~     (p=0.079
n=10+10)
CanonicalizePath      1.00 ± 0%      1.00 ± 0%    ~     (all equal)

util.go
@@ -62,9 +62,7 @@ func IsPathSeparator(c byte) bool {
 }
 
 // Canonicalize a path like "foo/../bar.h" into just "bar.h".
-// |slashBits| has bits set starting from lowest for a backslash that was
-// normalized to a forward slash. (only used on Windows)
-func CanonicalizePath(path string, slashBits *uint64) string {
+func CanonicalizePath(path string) string {
 	// TODO(maruel): Call site should be the lexers, so that it's done as a
 	// single pass.
 	// WARNING: this function is performance-critical; please benchmark
@@ -161,8 +159,112 @@ func CanonicalizePath(path string, slashBits *uint64) string {
 		dst += 2
 	}
 	p = p[:dst-1]
+	return unsafeString(p)
+}
+
+// Canonicalize a path like "foo/../bar.h" into just "bar.h".
+//
+// Returns a bits set starting from lowest for a backslash that was
+// normalized to a forward slash. (only used on Windows)
+func CanonicalizePathBits(path string) (string, uint64) {
+	// TODO(maruel): Call site should be the lexers, so that it's done as a
+	// single pass.
+	// WARNING: this function is performance-critical; please benchmark
+	// any changes you make to it.
+	l := len(path)
+	if l == 0 {
+		return path, 0
+	}
+
+	p := make([]byte, l+1)
+	copy(p, path)
+	// Tell the compiler that l is safe for p.
+	_ = p[l]
+	dst := 0
+	src := 0
+
+	if c := p[src]; c == '/' || c == '\\' {
+		if runtime.GOOS == "windows" && l > 1 {
+			// network path starts with //
+			if c := p[src+1]; c == '/' || c == '\\' {
+				src += 2
+				dst += 2
+			} else {
+				src++
+				dst++
+			}
+		} else {
+			src++
+			dst++
+		}
+	}
+
+	var components [60]int
+	for component_count := 0; src < l; {
+		if p[src] == '.' {
+			// It is fine to read one byte past because p is l+1 in
+			// length. It will be a 0 zero if so.
+			c := p[src+1]
+			if src+1 == l || (c == '/' || c == '\\') {
+				// '.' component; eliminate.
+				src += 2
+				continue
+			}
+			if c == '.' {
+				// It is fine to read one byte past because p is l+1 in
+				// length. It will be a 0 zero if so.
+				c := p[src+2]
+				if src+2 == l || (c == '/' || c == '\\') {
+					// '..' component.  Back up if possible.
+					if component_count > 0 {
+						dst = components[component_count-1]
+						src += 3
+						component_count--
+					} else {
+						p[dst] = p[src]
+						p[dst+1] = p[src+1]
+						p[dst+2] = p[src+2]
+						dst += 3
+						src += 3
+					}
+					continue
+				}
+			}
+		}
+
+		if c := p[src]; c == '/' || c == '\\' {
+			src++
+			continue
+		}
+
+		if component_count == len(components) {
+			Fatal("path has too many components : %s", path)
+		}
+		components[component_count] = dst
+		component_count++
+
+		for src != l {
+			c := p[src]
+			if c == '/' || c == '\\' {
+				break
+			}
+			p[dst] = c
+			dst++
+			src++
+		}
+		// Copy '/' or final \0 character as well.
+		p[dst] = p[src]
+		dst++
+		src++
+	}
+
+	if dst == 0 {
+		p[dst] = '.'
+		dst += 2
+	}
+	p = p[:dst-1]
+	bits := uint64(0)
 	if runtime.GOOS == "windows" {
-		bits := uint64(0)
 		bits_mask := uint64(1)
 		for i, c := range p {
 			switch c {
@@ -174,9 +276,8 @@ func CanonicalizePath(path string, slashBits *uint64) string {
 				bits_mask <<= 1
 			}
 		}
-		*slashBits = bits
 	}
-	return unsafeString(p)
+	return unsafeString(p), bits
 }
 
 func StringNeedsShellEscaping(input string) bool {

build.go
@@ -1081,9 +1081,7 @@ func (b *Builder) ExtractDeps(result *Result, deps_type string, deps_prefix stri
 		// XXX check depfile matches expected output.
 		//deps_nodes.reserve(deps.ins_.size())
 		for _, i := range deps.ins_ {
-			var slashBits uint64
-			i = CanonicalizePath(i, &slashBits)
-			*deps_nodes = append(*deps_nodes, b.state_.GetNode(i, slashBits))
+			*deps_nodes = append(*deps_nodes, b.state_.GetNode(CanonicalizePathBits(i)))
 		}
 
 		if !g_keep_depfile {

clean.go
@@ -250,8 +250,7 @@ func (c *Cleaner) CleanTargets(targets []string) int {
 			c.status_ = 1
 			continue
 		}
-		var slashBits uint64
-		target_name = CanonicalizePath(target_name, &slashBits)
+		target_name = CanonicalizePath(target_name)
 		target := c.state_.LookupNode(target_name)
 		if target != nil {
 			if c.IsVerbose() {

dyndep_parser.go
@@ -122,8 +122,7 @@ func (d *DyndepParser) ParseEdge(err *string) bool {
 		if len(path) == 0 {
 			return d.lexer_.Error("empty path", err)
 		}
-		var slashBits uint64
-		path = CanonicalizePath(path, &slashBits)
+		path = CanonicalizePath(path)
 		node := d.state_.LookupNode(path)
 		if node == nil || node.InEdge == nil {
 			return d.lexer_.Error("no build statement exists for '"+path+"'", err)
@@ -226,9 +225,7 @@ func (d *DyndepParser) ParseEdge(err *string) bool {
 		if len(path) == 0 {
 			return d.lexer_.Error("empty path", err)
 		}
-		var slashBits uint64
-		path = CanonicalizePath(path, &slashBits)
-		n := d.state_.GetNode(path, slashBits)
+		n := d.state_.GetNode(CanonicalizePathBits(path))
 		dyndeps.implicit_inputs_ = append(dyndeps.implicit_inputs_, n)
 	}
 
@@ -238,9 +235,7 @@ func (d *DyndepParser) ParseEdge(err *string) bool {
 		if len(path) == 0 {
 			return d.lexer_.Error("empty path", err)
 		}
-		var slashBits uint64
-		path = CanonicalizePath(path, &slashBits)
-		n := d.state_.GetNode(path, slashBits)
+		n := d.state_.GetNode(CanonicalizePathBits(path))
 		dyndeps.implicit_outputs_ = append(dyndeps.implicit_outputs_, n)
 	}
 	return true

graph.go
@@ -30,7 +30,7 @@ type Node struct {
 	Path string
 
 	// Set bits starting from lowest for backslashes that were normalized to
-	// forward slashes by CanonicalizePath. See |PathDecanonicalized|.
+	// forward slashes by CanonicalizePathBits. See |PathDecanonicalized|.
 	SlashBits uint64
 
 	// Mutable.
@@ -1023,14 +1023,11 @@ func (i *ImplicitDepLoader) LoadDepFile(edge *Edge, path string, err *string) bo
 		return false
 	}
 
-	var unused uint64
-	primary_out := CanonicalizePath(depfile.outs_[0], &unused)
-
 	// Check that this depfile matches the edge's output, if not return false to
 	// mark the edge as dirty.
 	first_output := edge.outputs_[0]
-	if first_output.Path != primary_out {
-		EXPLAIN("expected depfile '%s' to mention '%s', got '%s'", path, first_output.Path, primary_out)
+	if primaryOut := CanonicalizePath(depfile.outs_[0]); first_output.Path != primaryOut {
+		EXPLAIN("expected depfile '%s' to mention '%s', got '%s'", path, first_output.Path, primaryOut)
 		return false
 	}
 
@@ -1059,9 +1056,7 @@ func (i *ImplicitDepLoader) ProcessDepfileDeps(edge *Edge, depfile_ins []string,
 
 	// Add all its in-edges.
 	for _, j := range depfile_ins {
-		var slashBits uint64
-		j = CanonicalizePath(j, &slashBits)
-		node := i.state_.GetNode(j, slashBits)
+		node := i.state_.GetNode(CanonicalizePathBits(j))
 		edge.inputs_[implicit_dep] = node
 		node.AddOutEdge(edge)
 		i.CreatePhonyInEdge(node)

includes_normalize.go
@@ -167,8 +167,7 @@ func (i *IncludesNormalize) Normalize(input string, result *string, err *string)
 		*err = "path too long"
 		return false
 	}
-	var slashBits uint64
-	cp := CanonicalizePath(input, &slashBits)
+	cp := CanonicalizePath(input)
 	abs_input := AbsPath(cp, err)
 	if len(*err) != 0 {
 		return false

manifest_parser.go
@@ -233,10 +233,8 @@ func (m *ManifestParser) ParseDefault(err *string) bool {
 		if len(path) == 0 {
 			return m.lexer_.Error("empty path", err)
 		}
-		var slashBits uint64 // Unused because this only does lookup.
-		path = CanonicalizePath(path, &slashBits)
 		default_err := ""
-		if !m.state_.AddDefault(path, &default_err) {
+		if !m.state_.AddDefault(CanonicalizePath(path), &default_err) {
 			return m.lexer_.Error(default_err, err)
 		}
 
@@ -401,8 +399,7 @@ func (m *ManifestParser) ParseEdge(err *string) bool {
 		if len(path) == 0 {
 			return m.lexer_.Error("empty path", err)
 		}
-		var slashBits uint64
-		path = CanonicalizePath(path, &slashBits)
+		path, slashBits := CanonicalizePathBits(path)
 		if !m.state_.AddOut(edge, path, slashBits) {
 			if m.options_.dupe_edge_action_ == kDupeEdgeActionError {
 				m.lexer_.Error("multiple rules generate "+path, err)
@@ -430,8 +427,7 @@ func (m *ManifestParser) ParseEdge(err *string) bool {
 		if len(path) == 0 {
 			return m.lexer_.Error("empty path", err)
 		}
-		var slashBits uint64
-		path = CanonicalizePath(path, &slashBits)
+		path, slashBits := CanonicalizePathBits(path)
 		m.state_.AddIn(edge, path, slashBits)
 	}
 	edge.implicit_deps_ = implicit
@@ -443,8 +439,7 @@ func (m *ManifestParser) ParseEdge(err *string) bool {
 		if path == "" {
 			return m.lexer_.Error("empty path", err)
 		}
-		var slashBits uint64
-		path = CanonicalizePath(path, &slashBits)
+		path, slashBits := CanonicalizePathBits(path)
 		m.state_.AddValidation(edge, path, slashBits)
 	}
 
@@ -471,9 +466,7 @@ func (m *ManifestParser) ParseEdge(err *string) bool {
 	// be one of our manifest-specified inputs.
 	dyndep := edge.GetUnescapedDyndep()
 	if len(dyndep) != 0 {
-		var slashBits uint64
-		dyndep = CanonicalizePath(dyndep, &slashBits)
-		edge.dyndep_ = m.state_.GetNode(dyndep, slashBits)
+		edge.dyndep_ = m.state_.GetNode(CanonicalizePathBits(dyndep))
 		edge.dyndep_.set_dyndep_pending(true)
 		found := false
 		for _, n := range edge.inputs_ {

missing_deps.go
@@ -59,9 +59,7 @@ func NewNodeStoringImplicitDepLoader(state *State, deps_log *DepsLog, disk_inter
 
 func (n *NodeStoringImplicitDepLoader) ProcessDepfileDeps(edge *Edge, depfile_ins []string, err *string) bool {
 	for _, i := range depfile_ins {
-		var slashBits uint64
-		i = CanonicalizePath(i, &slashBits)
-		node := n.state_.GetNode(i, slashBits)
+		node := n.state_.GetNode(CanonicalizePathBits(i))
 		n.dep_nodes_output_ = append(n.dep_nodes_output_, node)
 	}
 	return true

commit da189991bbd023070c75f439fc4b848737d11556

Fix CanonicalizePath on Windows
Was broken in d30005bca3732d524e72d7d004166bc372a28504

util.go
@@ -159,6 +159,13 @@ func CanonicalizePath(path string) string {
 		dst += 2
 	}
 	p = p[:dst-1]
+	if runtime.GOOS == "windows" {
+		for i, c := range p {
+			if c == '\\' {
+				p[i] = '/'
+			}
+		}
+	}
 	return unsafeString(p)
 }
 