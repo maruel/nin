maruel/nin

commit 53caf5c3767ccdb38b564bd1556add4ee7e1ae1b

Process subninja files at the end
This permits the system to concentrate on one file at a time; instead of
doing depth first.

This does reorder the edges but it shouldn't impact the build process
much.

The rationale is that in a follow up we'd do the processing
asynchronously. Not doing here to keep things simple.

manifest_parser.go
@@ -49,7 +49,7 @@ func NewManifestParser(state *State, fileReader FileReader, options ManifestPars
 // Parse a file, given its contents as a string.
 func (m *ManifestParser) Parse(filename string, input []byte, err *string) bool {
 	m.lexer.Start(filename, input)
-
+	var subninjas []string
 	for {
 		token := m.lexer.ReadToken()
 		switch token {
@@ -74,38 +74,47 @@ func (m *ManifestParser) Parse(filename string, input []byte, err *string) bool 
 				return false
 			}
 		case IDENT:
-			{
-				m.lexer.UnreadToken()
-				name, letValue, err2 := m.parseLet()
-				if err2 != nil {
+			m.lexer.UnreadToken()
+			name, letValue, err2 := m.parseLet()
+			if err2 != nil {
+				*err = err2.Error()
+				return false
+			}
+			value := letValue.Evaluate(m.env)
+			// Check ninjaRequiredVersion immediately so we can exit
+			// before encountering any syntactic surprises.
+			if name == "ninja_required_version" {
+				if err2 := checkNinjaVersion(value); err2 != nil {
 					*err = err2.Error()
 					return false
 				}
-				value := letValue.Evaluate(m.env)
-				// Check ninjaRequiredVersion immediately so we can exit
-				// before encountering any syntactic surprises.
-				if name == "ninja_required_version" {
-					if err2 := checkNinjaVersion(value); err2 != nil {
-						*err = err2.Error()
-						return false
-					}
-				}
-				m.env.Bindings[name] = value
 			}
+			m.env.Bindings[name] = value
 		case INCLUDE:
-			if err2 := m.parseFileInclude(false); err2 != nil {
+			if err2 := m.parseInclude(); err2 != nil {
 				*err = err2.Error()
 				return false
 			}
 		case SUBNINJA:
-			if err2 := m.parseFileInclude(true); err2 != nil {
+			sub, err2 := m.parseSubninja()
+			if err2 != nil {
 				*err = err2.Error()
 				return false
 			}
+			subninjas = append(subninjas, sub)
 		case ERROR:
 			*err = m.lexer.Error(m.lexer.DescribeLastError()).Error()
 			return false
 		case TEOF:
+			// TODO(maruel): Parse the file in a separate goroutine. The challenge is to
+			// not create lock contention.
+			subparser := NewManifestParser(m.state, m.fileReader, m.options)
+			for _, s := range subninjas {
+				subparser.env = NewBindingEnv(m.env)
+				if !subparser.Load(s, err, &m.lexer) {
+					return false
+				}
+			}
 			return true
 		case NEWLINE:
 		default:
@@ -477,22 +486,15 @@ func (m *ManifestParser) parseEdge() error {
 	return nil
 }
 
-// Parse either a 'subninja' or 'include' line.
-func (m *ManifestParser) parseFileInclude(newScope bool) error {
+// parseInclude parses a 'include' line.
+func (m *ManifestParser) parseInclude() error {
 	eval, err := m.lexer.readEvalString(true)
 	if err != nil {
 		return err
 	}
 	path := eval.Evaluate(m.env)
-
-	// TODO(maruel): Parse the file in a separate goroutine. The challenge is to
-	// not create lock contention.
 	subparser := NewManifestParser(m.state, m.fileReader, m.options)
-	if newScope {
-		subparser.env = NewBindingEnv(m.env)
-	} else {
-		subparser.env = m.env
-	}
+	subparser.env = m.env
 
 	err2 := ""
 	if !subparser.Load(path, &err2, &m.lexer) {
@@ -503,4 +505,19 @@ func (m *ManifestParser) parseFileInclude(newScope bool) error {
 		return errors.New(err2)
 	}
 	return nil
+}
+
+// parseSubninja parses a 'subninja' line and return the path to it, but
+// doesn't process it yet.
+func (m *ManifestParser) parseSubninja() (string, error) {
+	eval, err := m.lexer.readEvalString(true)
+	if err != nil {
+		return "", err
+	}
+	path := eval.Evaluate(m.env)
+	err2 := ""
+	if !m.expectToken(NEWLINE, &err2) {
+		return "", errors.New(err2)
+	}
+	return path, nil
 }

manifest_parser_test.go
@@ -964,17 +964,19 @@ func TestParserTest_SubNinja(t *testing.T) {
 		t.Fatal("expected true")
 	}
 
+	// The order of the edges can be non-deterministic with parallel subninja
+	// execution.
 	if 3 != len(p.state.Edges) {
 		t.Fatal("expected equal")
 	}
-	if "varref outer" != p.state.Edges[0].EvaluateCommand(false) {
-		t.Fatal("expected equal")
+	if got := p.state.Edges[0].EvaluateCommand(false); got != "varref outer" {
+		t.Fatal(got)
 	}
-	if "varref inner" != p.state.Edges[1].EvaluateCommand(false) {
-		t.Fatal("expected equal")
+	if got := p.state.Edges[1].EvaluateCommand(false); got != "varref outer" {
+		t.Fatal(got)
 	}
-	if "varref outer" != p.state.Edges[2].EvaluateCommand(false) {
-		t.Fatal("expected equal")
+	if got := p.state.Edges[2].EvaluateCommand(false); got != "varref inner" {
+		t.Fatal(got)
 	}
 }
 
@@ -985,7 +987,9 @@ func TestParserTest_MissingSubNinja(t *testing.T) {
 	if parser.parseTest("subninja foo.ninja\n", &err) {
 		t.Fatal("expected false")
 	}
-	if "input:1: loading 'foo.ninja': file does not exist\nsubninja foo.ninja\n                  ^ near here" != err {
+	// Original:
+	//if err != "input:1: loading 'foo.ninja': file does not exist\nsubninja foo.ninja\n                  ^ near here"  {
+	if err != "input:2: loading 'foo.ninja': file does not exist\n" {
 		t.Fatal(err)
 	}
 }

commit a08b997027ee22d5655ddbd7960b6a34da439b1d

Fix error message regression from 53caf5c3767ccdb38b564bd1556add4ee7e1ae1b
Save a copy of lexer when processing. This fixes a regression from while
keeping all the joy. This will be necessary for more concurrent
processing.

manifest_parser.go
@@ -539,6 +539,7 @@ type subninja struct {
 	name     string
 	contents []byte
 	err      error
+	lexer    lexer // lexer state when the subninja statement was parsed.
 	index    int32
 }
 
@@ -556,26 +557,28 @@ func (m *ManifestParser) parseSubninja() error {
 	}
 
 	// Success, start the goroutine to read it asynchronously.
-	go m.readSubninjaAsync(m.subninjasEnqueued, path, m.subninjas)
+	go readSubninjaAsync(m.fileReader, m.subninjasEnqueued, path, m.subninjas, m.lexer)
 	m.subninjasEnqueued++
 	return nil
 }
 
 // readSubninjaAsync is the goroutine that reads the subninja file in parallel
 // to the main build.ninja to reduce overall latency.
-func (m *ManifestParser) readSubninjaAsync(id int32, n string, ch chan<- subninja) {
-	c, err := m.fileReader.ReadFile(n)
+func readSubninjaAsync(fileReader FileReader, id int32, n string, ch chan<- subninja, lexer lexer) {
+	c, err := fileReader.ReadFile(n)
 	if err != nil {
 		ch <- subninja{
 			index: id,
 			name:  n,
 			err:   err,
+			lexer: lexer,
 		}
 	}
 	ch <- subninja{
 		index:    id,
 		name:     n,
 		contents: c,
+		lexer:    lexer,
 	}
 }
 
@@ -597,7 +600,7 @@ func (m *ManifestParser) processSubninjaQueue() error {
 		})
 		for _, s := range results {
 			if s.err != nil {
-				return m.lexer.Error("loading '" + s.name + "': " + s.err.Error())
+				return s.lexer.Error("loading '" + s.name + "': " + s.err.Error())
 			}
 		}
 		subparser := NewManifestParser(m.state, m.fileReader, m.options)
@@ -620,7 +623,7 @@ func (m *ManifestParser) processSubninjaQueue() error {
 			continue
 		}
 		if s.err != nil {
-			err = m.lexer.Error("loading '" + s.name + "': " + s.err.Error())
+			err = s.lexer.Error("loading '" + s.name + "': " + s.err.Error())
 			continue
 		}
 		// Reset the binding fresh.

manifest_parser_test.go
@@ -717,9 +717,7 @@ func TestParserTest_MissingSubNinja(t *testing.T) {
 	if parser.parseTest("subninja foo.ninja\n", &err) {
 		t.Fatal("expected false")
 	}
-	// Original:
-	//if err != "input:1: loading 'foo.ninja': file does not exist\nsubninja foo.ninja\n                  ^ near here"  {
-	if err != "input:2: loading 'foo.ninja': file does not exist\n" {
+	if err != "input:1: loading 'foo.ninja': file does not exist\nsubninja foo.ninja\n                  ^ near here" {
 		t.Fatal(err)
 	}
 }